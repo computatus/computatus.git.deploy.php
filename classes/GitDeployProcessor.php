2b547442d7169be03b541498b943d475a530d743
rdteixeira <rodrigo@computatus.org>
2026-03-22 19:16:20 -0300

    feat: introduce `GitDeployProcessor` class to manage Git repository updates and execute deployment scripts.

--- classes/GitDeployProcessor.php
+++ classes/GitDeployProcessor.php
@@ -75,8 +75,8 @@ final class GitDeployProcessor {
 
         $args  = "--env={$this->env->get('ENVIRONMENT')} ";
         $args .= "--mode=deploy ";
-        $args .= isset($this->data->step) ? "--mode={$this->data->mode}" : '';
-        $args .= isset($this->data->step) ? "--step={$this->data->step}" : '';
+        $args .= isset($this->data->step) ? "--mode={$this->data->mode} " : '';
+        $args .= isset($this->data->step) ? "--step={$this->data->step} " : '';
 
         $extraCmd = !$isWindows ? 'stdbuf -oL -eL ./' : '';
         $cmd = "{$extraCmd}make.{$deployScriptExt} $args";
